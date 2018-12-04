26b415e36c740db80346295d5ed85e42bb683e0e
Crystal Espaillat <crystalespaillat@Crystals-MBP.home>
2018-12-04 09:32:23 -0500

    Commented out timeouts.

--- server.js
+++ server.js
@@ -100,8 +100,8 @@ app.post('/send', (req, res) => {
 
     var transporter = nodemailer.createTransport({
         service: GMAIL_SERVICE,
-        connectionTimeout : "7000",
-        greetingTimeout : "7000",
+        // connectionTimeout : "7000",
+        // greetingTimeout : "7000",
         auth
     });
 
