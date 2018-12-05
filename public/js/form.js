d344279046012c53c6bc70f9d1ba8ed9eb39497b
Crystal Espaillat <crystalespaillat@Crystals-MBP.home>
2018-12-05 09:51:43 -0500

    email doesnt send form values.

--- public/js/form.js
+++ public/js/form.js
@@ -2,21 +2,14 @@
 let name = $("#full-name").val().trim();
 let email = $("#email").val().trim();
 let plan = $("#plan").val();
-
-//Save the radio values
 let age = $('input[name="age"]:checked').val();
 let exercise =  $('input[name="exercise"]:checked').val();
-
-// Save the textarea value
 let struggle = $('textarea#struggle').val();
-
-// Save rating radio value:
 let rating= 0;
 for (i=1; i<=$(".rb").length; i++) {
     let rbValue = $(".rb-tab-active").attr("data-value");
     rating = parseInt(rbValue);
   };
-
 
 //Switcher function for rating:
 $(".rb-tab").click(() => {
@@ -37,13 +30,13 @@ $("form").parsley().on("form:submit", () => {
   $(".alert").show();
 
   // Test
-    // console.log("their name is: " + name);
-    // console.log("their email is: " + email);
-    // console.log("their plan is: " + plan);
-    // console.log("their age is: " + age);
-    // console.log("their exercise is: " + exercise);
-    // console.log("their struggle is: " + struggle);
-    // console.log("their rating is: " + rating);
+    console.log("their name is: " + name);
+    console.log("their email is: " + email);
+    console.log("their plan is: " + plan);
+    console.log("their age is: " + age);
+    console.log("their exercise is: " + exercise);
+    console.log("their struggle is: " + struggle);
+    console.log("their rating is: " + rating);
 
     let newApplication = {
         fullname: name,
